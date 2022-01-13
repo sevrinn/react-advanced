427f70c7b4406717a13bfc9ed18c498d7794fb2f
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-13 07:13:16 -0800

    useEffect basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/4-useState-object'
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
 function App() {
-	return (
-		<div className='container'>
-			<h2>Advanced Tutorial</h2>
-			<Setup />
-		</div>
-	)
+  return (
+    <div className='container'>
+      <h2>Advanced Tutorial</h2>
+      <Setup />
+    </div>
+  )
 }
 
 export default App
