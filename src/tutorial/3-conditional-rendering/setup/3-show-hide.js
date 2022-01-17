2fd6a3072f86c57598497c65370f70aa5c3fc0dd
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-17 14:03:23 -0800

    use effect clean up on a show/hide component

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
-};
+	//show is initially set to
+	const [show, setShow] = useState(false)
 
-export default ShowHide;
+	return (
+		<>
+			<button className='btn' onClick={() => setShow(!show)}>
+				show/hide
+			</button>
+			{/* circuited so if show is true, it will show item component */}
+			{show && <Item />}
+		</>
+	)
+}
+
+const Item = () => {
+	const [size, setSize] = useState(window.innerWidth)
+
+	const checkSize = () => {
+		setSize(window.innerWidth)
+	}
+	useEffect(() => {
+		window.addEventListener('resize', checkSize)
+		//MUST RETURN this cleanup function to remove the event listener
+		//just adding the empty dependency array will not work in this case
+		//because every time we toggle the component, it's rendered
+		return () => {
+			window.removeEventListener('resize', checkSize)
+		}
+	}, [])
+	return (
+		<>
+			<h1>Item</h1>
+			<p>The window size is: {size} pixels wide</p>
+		</>
+	)
+}
+
+export default ShowHide
